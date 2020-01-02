13a9e4644f0c3d927abba0534e50eab5b93485f9
César <cesar.watrin@hotmail.com>
2020-01-02 16:06:58 +0100

    Entrainement.php

--- Entrainement.php
+++ Entrainement.php
@@ -3,12 +3,59 @@
   <head>
     <meta charset="utf-8">
     <link rel="icon" href="icons/entrainement.png" />
+    <link rel="stylesheet" href="css/Entrainement.css">
     <title>Entrainement BOXE</title>
   </head>
   <body>
 
-   <h1>Page de sélection d'un entrainement</h1>
-   <h2>-Version Beta-</h2>
+
+
+
+
+      <fieldset>
+
+        <h1>Sélectionnez un type d'entrainement</h1>
+
+          <div class="services">
+                    <div class="service-item">
+                        <a href="Timer.php">
+                        <img src="img/demo.jpg" alt="Démo">
+                        <h4>Démo</h4>
+                        </a>
+                    </div>
+                    <div class="service-item">
+                        <a href="">
+                        <img src="img/endurance.jpg" alt="Endurance">
+                        <h4>Endurance</h4>
+                        </a>
+                    </div>
+                    <div class="service-item">
+                        <a href="">
+                        <img src="img/musculation.jpg" alt="Musculation">
+                        <h4>Musculation</h4>
+                        </a>
+                    </div>
+                    <div class="service-item">
+                        <a href="">
+                        <img src="img/mouvements.jpg" alt="Mouvements">
+                        <h4>Mouvements</h4>
+                        </a>
+                    </div>
+          </div>
+
+          <section class="bouton">
+            <a href="Entrainement.php">
+              <span class="text">Quitter</span>
+              <span class="line -right"></span>
+              <span class="line -top"></span>
+              <span class="line -left"></span>
+              <span class="line -bottom"></span>
+            </a>
+          </section>
+
+    </fieldset>
+
+
 
 
 
